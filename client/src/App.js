6e9c6d6e42e13d87fe7afbd95e1825008c42662b
Robyn Tyler <robynstyler@outlook.com>
2021-01-10 16:47:01 +0000

    Map and Infobox components displaying in browser

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {useState, useEffect} from "react";
 import {getWeekData} from "./services/MapService";
 import EarthquakeInfoBox from "./components/EarthquakeInfoBox";
+import EarthquakeMap from "./components/EarthquakeMap";
 
 function App() {
   const [realtimeQuakes, setRealtimeQuakes] = useState([]);
@@ -16,7 +17,8 @@ function App() {
   return (
     <>
     <h1>QUICK, IT'S A QUAKE</h1>
-    <EarthquakeInfoBox
+    <EarthquakeInfoBox/>
+    <EarthquakeMap/>
     </>
   );
 }
