348decc172197633cdd3f05996bc8be540fa72d5
Mark Richardson <mark_richardson@sky.com>
2021-01-11 13:21:40 +0000

    Map showing all recent quakes now showing

--- client/src/components/EarthquakeMap.js
+++ client/src/components/EarthquakeMap.js
@@ -6,26 +6,43 @@ import 'leaflet/dist/leaflet.css';
 
 // coordinates on usgs longlatdepth, removing depth and switching to Lat Long for Leaflet
 const EarthquakeMap = ({realtimeQuakes}) => {
-    const earthquakeNodes = realtimeQuakes.map((quake => {
-        const posLonLat = quake.geometry.coordinates.slice(0,2)
-        const LatLong = posLonLat.reverse()
-        console.log(posLonLat)
-        console.log(LatLong)
-        return(
-            <>
-            <Earthquake quake={quake} key={quake.id} LatLong={LatLong} />
-            </>
-        )
-
-    }))
+//     const earthquakeNodes = realtimeQuakes.map((quake => {
+//         const posLonLat = quake.geometry.coordinates.slice(0,2)
+//         const LatLong = posLonLat.reverse()
+//         console.log(posLonLat)
+//         console.log(LatLong)
+//         return(
+//             <>
+//             {LatLong}
+//             {/* <Earthquake quake={quake} key={quake.id} LatLong={LatLong} /> */}
+//             </>
+//         )
+//     }))
 
 
     return(
         <>
         <p>This is the map, BOOYAH!</p>
-        {earthquakeNodes}
-        
-        </>
+        {/* {earthquakeNodes} */}
+         <MapContainer 
+              className="map"
+              center={[0, 0]}
+              zoom={4}
+              style={{ height: 450, width: 450 }}
+               >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors" /> 
+
+           {realtimeQuakes.map(quake => ( 
+            <Marker position={[quake.geometry.coordinates[1], quake.geometry.coordinates[0]]} key={quake.id} >
+             <Popup>
+              This was an earthquake called {quake.properties.title} with a magnitude of {}.
+            </Popup>
+            {console.log(quake.geometry.coordinates.slice(0,2).reverse())}
+            </Marker>
+           ))} 
+     </MapContainer> 
+        </> 
     )
 }
 
