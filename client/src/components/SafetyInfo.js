b7ec7cff7b60e516ba29ae53f4671053cb9990f3
Mark Richardson <mark.richardson4@gmail.com>
2021-01-13 15:10:10 +0000

    Updating quake safety info and checking display

--- client/src/components/SafetyInfo.js
+++ client/src/components/SafetyInfo.js
@@ -14,8 +14,18 @@ const SafetyInfo = () => {
 
     <p className="header2">DURING</p>
     <p className="header3">Indoors:</p>
-    <p>“DROP, COVER and HOLD ON!“</p>
-<p>“Move as little as possible - most injuries during earthquakes occur because of people moving around, falling and suffering sprains, fractures and head injuries.“, “Try to protect your head and torso.“, “If you are in bed, stay there, curl up and hold on, and cover your head.“, “Stay indoors until the shaking stops and you are sure it is safe to exit.“, “If you must leave a building after the shaking stops, use stairs rather than an elevator in case of aftershocks, power outages or other damage.“, “Be aware that smoke alarms and sprinkler systems frequently go off in buildings during an earthquake, even if there is no fire.“, “If you smell gas, get out of the house and move as far away as possible.“, “Before you leave any building check to make sure that there is no debris from the building that could fall on you.“,</p>
+    <p>DROP, COVER and HOLD ON!</p>
+    <ul>
+      <li>Move as little as possible - most injuries during earthquakes occur because of people moving around, falling and suffering sprains, fractures and head injuries.</li>
+      <li>Try to protect your head and torso.</li>
+      <li>If you are in bed, stay there, curl up and hold on, and cover your head.</li>
+      <li>Stay indoors until the shaking stops and you are sure it is safe to exit.</li>
+      <li>If you must leave a building after the shaking stops, use stairs rather than an elevator in case of aftershocks, power outages or other damage. </li>
+      <li>Be aware that smoke alarms and sprinkler systems frequently go off in buildings during an earthquake, even if there is no fire. </li>
+      <li>If you smell gas, get out of the house and move as far away as possible.</li>
+      <li>Before you leave any building check to make sure that there is no debris from the building that could fall on you.</li>
+      <li>Doorways are no stronger than any other part of a structure so don’t rely on them for protection! During an earthquake, get under a sturdy piece of furniture and hold on.</li>
+  </ul>
 <p className="header3">Outdoors:</p>
 <p>“Find a clear spot and drop to the ground. Stay there until the shaking stops.“, “Try to get as far away from buildings, power lines, trees, and streetlights as possible.“, “If you’re in a vehicle, pull over to a clear location and stop. Avoid bridges, overpasses and power lines if possible.“, “Stay inside with your seatbelt fastened until the shaking stops.“, “After the shaking has stopped, drive on carefully, avoiding bridges and ramps that may have been damaged.“, “If a power line falls on your vehicle, do not get out. Wait for assistance.“, “If you are in a mountainous area or near unstable slopes or cliffs, be alert for falling rocks and other debris as well as landslides.“</p>
 
