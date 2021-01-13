1cfdbc15b0362ae49c355c41590b928995361c30
Mark Richardson <mark.richardson4@gmail.com>
2021-01-13 15:30:20 +0000

    Tidying up formatting in safetyinfo file

--- client/src/components/SafetyInfo.js
+++ client/src/components/SafetyInfo.js
@@ -3,51 +3,48 @@ const SafetyInfo = () => {
 
   return (
     <div className="safety-info">
-    <p className="header2">For your safety</p>
-    
-
-    <p className="header2">BEFORE</p>
-    <p className="header3">Prepare in Advance</p>
-    <p>Be sure you’re Red Cross Ready. That means...</p>
-    <ul>
-      <li>Assembling an <a href="https://www.redcross.org/get-help/how-to-prepare-for-emergencies/survival-kit-supplies.html" target="_blank" data-aa-link-button="emergency-preparedness-kit">emergency preparedness kit</a>.</li>
-      <li>Creating a <a href="https://www.redcross.org/get-help/how-to-prepare-for-emergencies/make-a-plan.html" target="_blank" data-aa-link-button="household-evacuation-plan">household evacuation plan</a> that includes your pets.</li>
-      <li>Staying informed about your community’s risk and response plans.</li>
-      <li>Ensuring each family member knows <a href="https://www.redcross.org/get-help/disaster-relief-and-recovery-services/contact-and-locate-loved-ones.html" target="_blank" data-aa-link-button="how-to-get-back-in-touch-if-you-are-separated">how to get back in touch if you are separated</a> during an emergency.</li>
-      <li>Download the Emergency App for <a href="https://itunes.apple.com/us/app/emergency-by-american-red/id954783878?mt=8" target="_blank" data-aa-link-button="iPhone->>">iPhone</a> or for <a href="https://play.google.com/store/apps/details?id=com.cube.arc.hzd" target="_blank" data-aa-link-button="Android->>">Android</a>.</li>
-    </ul>
+      <p className="header2">For your safety</p>
+      <p className="header2">BEFORE</p>
+      <p className="header3">Prepare in Advance</p>
+      <p>Be sure you’re Red Cross Ready. That means...</p>
+      <ul>
+        <li>Assembling an <a href="https://www.redcross.org/get-help/how-to-prepare-for-emergencies/survival-kit-supplies.html" target="_blank" data-aa-link-button="emergency-preparedness-kit">emergency preparedness kit</a>.</li>
+        <li>Creating a <a href="https://www.redcross.org/get-help/how-to-prepare-for-emergencies/make-a-plan.html" target="_blank" data-aa-link-button="household-evacuation-plan">household evacuation plan</a> that includes your pets.</li>
+        <li>Staying informed about your community’s risk and response plans.</li>
+        <li>Ensuring each family member knows <a href="https://www.redcross.org/get-help/disaster-relief-and-recovery-services/contact-and-locate-loved-ones.html" target="_blank" data-aa-link-button="how-to-get-back-in-touch-if-you-are-separated">how to get back in touch if you are separated</a> during an emergency.</li>
+        <li>Download the Emergency App for <a href="https://itunes.apple.com/us/app/emergency-by-american-red/id954783878?mt=8" target="_blank" data-aa-link-button="iPhone->>">iPhone</a> or for <a href="https://play.google.com/store/apps/details?id=com.cube.arc.hzd" target="_blank" data-aa-link-button="Android->>">Android</a>.</li>
+      </ul>
 
 
-    <p className="header2">DURING</p>
-    <p className="header3">Indoors:</p>
-    <p>DROP, COVER and HOLD ON!</p>
-    <ul>
-      <li>Move as little as possible - most injuries during earthquakes occur because of people moving around, falling and suffering sprains, fractures and head injuries.</li>
-      <li>Try to protect your head and torso.</li>
-      <li>Doorways are no stronger than any other part of a structure so don’t rely on them for protection! During an earthquake, get under a sturdy piece of furniture and hold on.</li>
-      <li>If you are in bed, stay there, curl up and hold on, and cover your head.</li>
-      <li>Stay indoors until the shaking stops and you are sure it is safe to exit.</li>
-      <li>If you must leave a building after the shaking stops, use stairs rather than an elevator in case of aftershocks, power outages or other damage. </li>
-      <li>Be aware that smoke alarms and sprinkler systems frequently go off in buildings during an earthquake, even if there is no fire. </li>
-      <li>If you smell gas, get out of the house and move as far away as possible.</li>
-      <li>Before you leave any building check to make sure that there is no debris from the building that could fall on you.</li>
+      <p className="header2">DURING</p>
+      <p className="header3">Indoors:</p>
+      <p><strong>DROP, COVER and HOLD ON!</strong></p>
+      <ul>
+        <li>Move as little as possible - most injuries during earthquakes occur because of people moving around, falling and suffering sprains, fractures and head injuries.</li>
+        <li>Try to protect your head and torso.</li>
+        <li>Doorways are no stronger than any other part of a structure so don’t rely on them for protection! During an earthquake, get under a sturdy piece of furniture and hold on.</li>
+        <li>If you are in bed, stay there, curl up and hold on, and cover your head.</li>
+        <li>Stay indoors until the shaking stops and you are sure it is safe to exit.</li>
+        <li>If you must leave a building after the shaking stops, use stairs rather than an elevator in case of aftershocks, power outages or other damage. </li>
+        <li>Be aware that smoke alarms and sprinkler systems frequently go off in buildings during an earthquake, even if there is no fire. </li>
+        <li>If you smell gas, get out of the house and move as far away as possible.</li>
+        <li>Before you leave any building check to make sure that there is no debris from the building that could fall on you.</li>
+      </ul>
+      <p className="header3">Outdoors:</p>
+      <ul>
+        <li>Find a clear spot and drop to the ground. Stay there until the shaking stops.</li>
+        <li>Try to get as far away from buildings, power lines, trees, and streetlights as possible.</li>
+        <li>If you're in a vehicle, pull over to a clear location and stop. Avoid bridges, overpasses and power lines if possible.</li>
+        <li>Stay inside with your seatbelt fastened until the shaking stops.</li>
+        <li>After the shaking has stopped, drive on carefully, avoiding bridges and ramps that may have been damaged.</li>
+        <li>If a power line falls on your vehicle, do not get out. Wait for assistance.</li>
+        <li>If you are in a mountainous area or near unstable slopes or cliffs, be alert for falling rocks and other debris as well as <a href="https://www.redcross.org/get-help/how-to-prepare-for-emergencies/types-of-emergencies/landslide.html" target="_blank" data-aa-link-button="landslides">landslides</a>.</li>
+      </ul>
 
-  </ul>
-<p className="header3">Outdoors:</p>
-  <ul>
-    <li>Find a clear spot and drop to the ground. Stay there until the shaking stops.</li>
-    <li>Try to get as far away from buildings, power lines, trees, and streetlights as possible.</li>
-    <li>If you're in a vehicle, pull over to a clear location and stop. Avoid bridges, overpasses and power lines if possible.</li>
-    <li>Stay inside with your seatbelt fastened until the shaking stops.</li>
-    <li>After the shaking has stopped, drive on carefully, avoiding bridges and ramps that may have been damaged.</li>
-    <li>If a power line falls on your vehicle, do not get out. Wait for assistance.</li>
-    <li>If you are in a mountainous area or near unstable slopes or cliffs, be alert for falling rocks and other debris as well as <a href="https://www.redcross.org/get-help/how-to-prepare-for-emergencies/types-of-emergencies/landslide.html" target="_blank" data-aa-link-button="landslides">landslides</a>.</li>
-  </ul>
 
-
-    <p className="header2">AFTER</p>
-    <p className="header3">Staying Safe After an Earthquake</p> 
-    <p><strong>If you do nothing else:</strong></p>
+      <p className="header2">AFTER</p>
+      <p className="header3">Staying Safe After an Earthquake</p>
+      <p><strong>If you do nothing else:</strong></p>
       <ol>
         <li>If away from home, return only when authorities say it is safe to do so.</li>
         <li>Check yourself for injuries and get first aid, if necessary, before helping injured or trapped persons.</li>
