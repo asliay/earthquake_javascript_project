33654688f96379db6778f7682c9e15dbf0b7ee45
Mark Richardson <mark.richardson4@gmail.com>
2021-01-13 15:18:25 +0000

    Updating quake safety preparation info

--- client/src/components/SafetyInfo.js
+++ client/src/components/SafetyInfo.js
@@ -7,9 +7,15 @@ const SafetyInfo = () => {
     
 
     <p className="header2">BEFORE</p>
-    <p className="header3">PREPARE YOURSELF, FOOLS</p>
-    <p>Make sure you're ready... WIBBLYWOBBLIES ARE COMING</p>
-
+    <p className="header3">Prepare in Advance</p>
+    <p>Be sure you’re Red Cross Ready. That means...</p>
+    <ul>
+      <li>Assembling an <a href="https://www.redcross.org/get-help/how-to-prepare-for-emergencies/survival-kit-supplies.html" target="_blank" data-aa-link-button="emergency-preparedness-kit">emergency preparedness kit</a>.</li>
+      <li>Creating a <a href="https://www.redcross.org/get-help/how-to-prepare-for-emergencies/make-a-plan.html" target="_blank" data-aa-link-button="household-evacuation-plan">household evacuation plan</a> that includes your pets.</li>
+      <li>Staying informed about your community’s risk and response plans.</li>
+      <li>Ensuring each family member knows <a href="https://www.redcross.org/get-help/disaster-relief-and-recovery-services/contact-and-locate-loved-ones.html" target="_blank" data-aa-link-button="how-to-get-back-in-touch-if-you-are-separated">how to get back in touch if you are separated</a> during an emergency.</li>
+      <li>Download the Emergency App for <a href="https://itunes.apple.com/us/app/emergency-by-american-red/id954783878?mt=8" target="_blank" data-aa-link-button="iPhone->>">iPhone</a> or for <a href="https://play.google.com/store/apps/details?id=com.cube.arc.hzd" target="_blank" data-aa-link-button="Android->>">Android</a>.</li>
+    </ul>
 
 
     <p className="header2">DURING</p>
@@ -18,13 +24,14 @@ const SafetyInfo = () => {
     <ul>
       <li>Move as little as possible - most injuries during earthquakes occur because of people moving around, falling and suffering sprains, fractures and head injuries.</li>
       <li>Try to protect your head and torso.</li>
+      <li>Doorways are no stronger than any other part of a structure so don’t rely on them for protection! During an earthquake, get under a sturdy piece of furniture and hold on.</li>
       <li>If you are in bed, stay there, curl up and hold on, and cover your head.</li>
       <li>Stay indoors until the shaking stops and you are sure it is safe to exit.</li>
       <li>If you must leave a building after the shaking stops, use stairs rather than an elevator in case of aftershocks, power outages or other damage. </li>
       <li>Be aware that smoke alarms and sprinkler systems frequently go off in buildings during an earthquake, even if there is no fire. </li>
       <li>If you smell gas, get out of the house and move as far away as possible.</li>
       <li>Before you leave any building check to make sure that there is no debris from the building that could fall on you.</li>
-      <li>Doorways are no stronger than any other part of a structure so don’t rely on them for protection! During an earthquake, get under a sturdy piece of furniture and hold on.</li>
+
   </ul>
 <p className="header3">Outdoors:</p>
 <p>“Find a clear spot and drop to the ground. Stay there until the shaking stops.“, “Try to get as far away from buildings, power lines, trees, and streetlights as possible.“, “If you’re in a vehicle, pull over to a clear location and stop. Avoid bridges, overpasses and power lines if possible.“, “Stay inside with your seatbelt fastened until the shaking stops.“, “After the shaking has stopped, drive on carefully, avoiding bridges and ramps that may have been damaged.“, “If a power line falls on your vehicle, do not get out. Wait for assistance.“, “If you are in a mountainous area or near unstable slopes or cliffs, be alert for falling rocks and other debris as well as landslides.“</p>
