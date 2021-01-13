cb4a8f3e9a05d21f4b37548373565cd1904e61af
Robyn Tyler <robynstyler@outlook.com>
2021-01-13 14:00:40 +0000

    Added basic styles to Safety info box.

--- client/src/components/SafetyInfo.js
+++ client/src/components/SafetyInfo.js
@@ -13,7 +13,7 @@ const SafetyInfo = ({quakeInfo}) => {
     <p>DURING</p>
     <h2>Indoors: “DROP, COVER and HOLD ON!“,</h2>
 <p>“Move as little as possible - most injuries during earthquakes occur because of people moving around, falling and suffering sprains, fractures and head injuries.“, “Try to protect your head and torso.“, “If you are in bed, stay there, curl up and hold on, and cover your head.“, “Stay indoors until the shaking stops and you are sure it is safe to exit.“, “If you must leave a building after the shaking stops, use stairs rather than an elevator in case of aftershocks, power outages or other damage.“, “Be aware that smoke alarms and sprinkler systems frequently go off in buildings during an earthquake, even if there is no fire.“, “If you smell gas, get out of the house and move as far away as possible.“, “Before you leave any building check to make sure that there is no debris from the building that could fall on you.“,</p>
-<p>Outdoors:</p>
+<h2>Outdoors:</h2>
 <p>“Find a clear spot and drop to the ground. Stay there until the shaking stops.“, “Try to get as far away from buildings, power lines, trees, and streetlights as possible.“, “If you’re in a vehicle, pull over to a clear location and stop. Avoid bridges, overpasses and power lines if possible.“, “Stay inside with your seatbelt fastened until the shaking stops.“, “After the shaking has stopped, drive on carefully, avoiding bridges and ramps that may have been damaged.“, “If a power line falls on your vehicle, do not get out. Wait for assistance.“, “If you are in a mountainous area or near unstable slopes or cliffs, be alert for falling rocks and other debris as well as landslides.“</p>
     <p></p>
     <p></p>
