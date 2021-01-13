348688b798db3ac47006edcd7969942a015c3174
Ailsa Young <ailsa.young16@hotmail.co.uk>
2021-01-13 10:46:48 +0000

    added general info to seeds.js

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -668,7 +668,31 @@ db.historicalEarthquakes.insertMany([
 
 db.earthquakeInfo.insertMany([
   { 
-    generalInfo: "info goes here",
+    generalInfo: `An earthquake is the shaking of the Earth’s surface, usually due to movement of the planet’s huge tectonic plates. When these plates move and grind against each other along fault lines they sometimes get stuck. Pressure builds up and once released, seismic waves are sent through the Earth.  The point at which the energy is released is called the focus (or hypocenter). The position on the Earth’s surface directly above the focus is the epicentre. Seismic waves spread out from the focus, being felt most strongly at the epicentre. Most damage caused by a severe earthquake will happen close to this point. 
+
+    How are earthquakes detected?
+      Seismogram/ Seismometer
+      
+    How are earthquakes measured?
+      Earthquakes are measured
+    
+      The two most common scales used by seismologists are magnitude and intensity:
+      
+      Most common measure of magnitude is the Richter Scale.
+      Magnitude scales measure the overall strength of an earthquake, which may or may not cause perceptible shaking.
+    
+      The magnitude of an earthquake is related to the length of the fault on which it occurs. While a magnitude 10 earthquake is 	theoretically possible, no fault long enough to generate one is known to exist, and would need to extend around most of the 	planet.
+    
+      Several intensity scales exist and are used in different parts of the world. They categorise the intensity of ground shaking at a 	given location based on the observed effects, such as the degree to which people are harmed, and the severity of damage to 	structures or natural features.
+  
+    How often do they happen?
+    
+      The U.S. Geological Survey estimates that several million earthquakes occur in the world each year. Many are undetected because they occur in remote 	areas or have such a low magnitude that they aren’t felt. Estimations suggest around 150 earthquakes of magnitude 6 and higher occur each year, though 	magnitude 7 and above are much rarer still.
+    
+      The largest earthquake (by magnitude) every recorded was a magnitude 9.5 on May 22nd 1960 in Chile, rated IX (Violent) on the Modified Mercalli intensity scale. 
+    
+      While a magnitude 10 earthquake is theoretically possible, no fault long enough to generate one is known to exist. The magnitude of an earthquake is 	related to the length of the fault from which it originates, and a fault long enough to produce a magnitude 10 earthquake would need to extend around most 	of the planet.`
+      ,
     safetyInfo: "how to be safe during an earthquake!",
     historical: {
       funFacts: {
